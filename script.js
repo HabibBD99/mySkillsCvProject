6c9dbd222d891dce4ee71d3163c6b5c4d3234418
Habib <70913270+HabibBD99@users.noreply.github.com>
2024-09-06 22:28:18 +0600

    new code added
    header code updated

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+let Header_A = document.querySelectorAll(".Header_A");
+    Header_A.addEventListener("mouseover",function(){
+	alert("Hi");
+	
+	});
+let Address_Span = document.querySelectorAll(".Address_Span");
+
+if(Header_A.style.color="orangered";){
+	Address_Span.style.color="orangered";
+	
+}
+
